9b9ebae34ab1339f3793459141302b0458f2f09d
Isabelle Heloisa <91278843+IsabelleHeloisa@users.noreply.github.com>
2021-10-22 02:38:27 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+  github: 'IsabelleHeloisa',
+  youtube: 'UCzTaf8YOmuG_M3CIgS6hvZQ',
+  instagram: 'izzy_souza4',
+  twitter: 'Isabelle_Souza2'
+}
+
+function changeSocialMediaLinks() {
+  for (let li of socialLinks.children) {
+    const social = li.getAttribute('class')
+
+    li.children[0].href = `https//${social}.com/${linksSocialMedia[social]}`
+  }
+}
+
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos() {
+  const url = `https://api.github.com/users/${linksSocialMedia.github}`
+
+  fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      userName.textContent = data.name
+      userBio.textContent = data.bio
+      userLink.href = data.html_url
+      userPhoto.src = data.avatar_url
+      userLogin.textContent = data.login
+    })
+}
+
+getGitHubProfileInfos()
